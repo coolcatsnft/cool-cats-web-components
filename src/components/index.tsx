8ee9cad170ad10d78dd604de22bda8917f96fc33
= <=>
2022-04-07 07:03:21 +0100

    chore: add icon and opensea icon components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -17,6 +17,7 @@ export { default as ItemSummary } from "./ItemSummary";
 export { default as Panel } from "./Panel";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as MilkValue } from "./MilkValue";
+export { default as Icon } from "./Icon";
 
 // Form
 export { Legend as FormLegend } from "./Form";
@@ -38,3 +39,4 @@ export { Envelope as EnvelopeIcon } from "./Icon";
 export { Meowpad as MeowpadIcon } from "./Icon";
 export { Help as HelpIcon } from "./Icon";
 export { Milk as MilkIcon } from "./Icon";
+export { Opensea as OpenseaIcon } from "./Icon";
