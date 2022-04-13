b49ec405aa9b1ff5dc6f729c5ed8b12390aea9d8
Alex <alex@coolcatsnft.com>
2022-04-13 17:03:20 +0100

    chore: add missing import/export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -21,6 +21,8 @@ export { default as Icon } from "./Icon";
 export { default as Stats } from "./Stats";
 export { StatTable } from "./Stats";
 export { default as RarityBadge } from "./RarityBadge";
+export { default as InputButton } from "./InputButton";
+export { default as Button } from "./Button";
 
 // Form
 export { Legend as FormLegend } from "./Form";
