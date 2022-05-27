8bd02e056e26bdc19a6a314a5fe19b02df2f31e3
Alex-CoolCats <alex@coolcatsnft.com>
2022-05-27 08:49:45 +0000

    Deploying to release from @ Alex-CoolCats/cool-cats-web-components@c226140fd9501138b78c90752441af4bef0323ed 🚀

--- src/components/Form/Input.stories.tsx
+++ src/components/Form/Input.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getSizes } from '../../utils';
-import { Input}  from './Input';
+import { Input } from './Input';
 
 export default {
   title: 'Form/TextInput',
@@ -20,30 +20,35 @@ export default {
 
 export const BasicText = ((args) => {
   return (
-    <Input name='test' label="Basic Input" {...args} />
+    <Input name="test" label="Basic Input" {...args} />
   )
 }).bind({});
 
 export const BasicDisabledText = ((args) => {
   return (
-    <Input name='test' label="Basic Disabled Input" {...args} disabled/>
+    <Input name="test" label="Basic Disabled Input" {...args} disabled />
   )
 }).bind({});
 
 export const BasicTextWithValidation = ((args) => {
   return (
-    <Input name='test' label="Basic Input" {...args} error="Required" required/>
+    <>
+      <Input name="test" label="Basic Input" {...args} error="test" required defaultValue="test"/>
+      <Input name="test1" label="Basic Input" {...args} required />
+      <Input name="test1" label="Basic Input" {...args} required />
+      <Input name="test1" label="Basic Input" {...args} error="Required" required />
+    </>
   )
 }).bind({});
 
 export const BasicNumber = ((args) => {
   return (
-    <Input name='number field' label="Number" type="number" min={0} max={10} {...args} />
+    <Input name="number field" label="Number" type="number" min={0} max={10} {...args} />
   )
 }).bind({});
 
 export const Textarea = ((args) => {
   return (
-    <Input name='textarea' label='Text area' type='textarea' {...args} max={250}/>
+    <Input name="textarea" label="Text area" type="textarea" {...args} max={250} />
   )
 }).bind({});
